726f6830b58da671aaaf939ea35c9e24b0f1c118
gbarcia <gerardobarciap@gmail.com>
2010-11-29 17:16:34 -0430

    Adicion de soporte para colores de las iteraciones del roadmap Eliminacion de color para milestone

--- app/helpers/milestones_helper.rb
+++ app/helpers/milestones_helper.rb
@@ -1,2 +1,7 @@
 module MilestonesHelper
+  def color_is_selected? (actual_color, select_option)
+    if (actual_color == select_option)
+    return "selected='" + (actual_color == select_option).to_s + "'"
+    end
+  end
 end
